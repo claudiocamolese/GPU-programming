e1e646f859b5c356105f6558573ea1abc6ed7794
claudiocamolese <claudiocamolese@gmail.com>
2025-10-05 10:41:18 +0200

    add

---
+++ prove/addition.cu
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void add(int n, float *x, float *y){
+
+    for(int i = 0; i<n; i++){
+        y[i] = x[i] + y[i];
+    }
+}
+
+
+int main(void){
+
+    // Declaration
+
+    int N = 1<<20;
+    float *x = new float[N];
+    float *y = new float[N];
+
+    //CPU: initialize x and y
+
+    for(int i =0; i<N; i++0){
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // CPU: run on 1M elements
+    add(N, x, y);
+
+    delete [] x;
+    delete [] y;
+
+    return 0;
+
+}
