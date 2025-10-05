d89fcd6018fafd34ad380cc66acd27fdb44e0228
claudiocamolese <claudiocamolese@gmail.com>
2025-10-05 10:58:39 +0200

    f

--- prove/addition.cu
+++ prove/addition.cu
@@ -1,9 +1,9 @@
 #include <iostream>
 
-void add(int n, float *x, float *y){
+void add(int n, float *x, float *y, float*f){
 
     for(int i = 0; i<n; i++){
-        y[i] = x[i] + y[i];
+        f[i] = x[i] + y[i];
     }
 }
 
@@ -15,6 +15,7 @@ int main(void){
     int N = 1<<20;
     float *x = new float[N];
     float *y = new float[N];
+    float *f = new float[N]
 
     //CPU: initialize x and y
 
@@ -24,10 +25,12 @@ int main(void){
     }
 
     // CPU: run on 1M elements
-    add(N, x, y);
+    add(N, x, y, f);
+
 
     delete [] x;
     delete [] y;
+    delete [] f;
 
     return 0;
 
