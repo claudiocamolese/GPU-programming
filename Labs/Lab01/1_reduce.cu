69ccd54934e51038dd927227284382d06f9ec030
claudiocamolese <claudiocamolese@gmail.com>
2025-11-08 17:45:15 +0100

    reduce2

--- Labs/Lab01/1_reduce.cu
+++ Labs/Lab01/1_reduce.cu
@@ -2,6 +2,15 @@
 Reduce algorithm 1: Parallel sum of N numbers:
     - Use N/2 threads to get N/2 pairwise sums
     - Set N=N/2 and iterate till N=1
+
+To improve:
+    - in x[t] += x[t + m]; we need to load both x[tid] and x[tid+m] into GPU registers and then storing it in x[tid]
+        If we could accumulate partials sums in local registers, that would reudce the number of global memory access, which offers a speed up.
+    
+    - the host calls the kernel iteatively,halving the array size at each step and leaving the sum in the first array element.
+        If we could instead perform the iteration inside the kernel that could also reduce the number of memory accesses required.
+    
+    - the array must be a multiple of 2
 */
 
 #include <stdio.h>
@@ -46,11 +55,19 @@ int main(int argc, char const *argv[])
     Reducing algorithm in GPU
     */
    auto start2 = std::chrono::high_resolution_clock::now();
+
    for(int m = N/2; m > 0; m /= 2){
+    /*
+    m thread because there are m sum to compute but there is a max number of threads for blocks (1024 or 256 in this case).
+    if m >256, take 256.
+    if m <256, take m since is secure as number.
+    */
     int threads = std::min(256,m);
     int blocks = std::max(m/256,1);
+
     reduce<<<blocks, threads>>>(dev_x.data().get(), m);
-    }
+   }
+
     cudaDeviceSynchronize();
     auto end2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> t2 = end2 - start2;
