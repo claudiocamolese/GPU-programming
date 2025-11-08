59283fe047b395b1b81670c22016dfccb7e6e5b0
claudiocamolese <claudiocamolese@gmail.com>
2025-11-08 15:55:00 +0100

    chrono

--- Labs/Lab01/1_reduce.cu
+++ Labs/Lab01/1_reduce.cu
@@ -7,11 +7,11 @@ Reduce algorithm 1: Parallel sum of N numbers:
 #include <stdio.h>
 #include <stdlib.h>
 #include<cuda_runtime.h>
-#include "._cxtimers.h"
 #include<math.h>
 #include<thrust/device_vector.h>
 #include<thrust/host_vector.h>
 #include <random>
+#include <chrono>
 
 
 __global__ void reduce(float * x, int m){
@@ -36,12 +36,11 @@ int main(int argc, char const *argv[])
 
     dev_x = host_x;
     
-    cx::timer tim;
+    auto start = std::chrono::high_resolution_clock::now();
     double host_sum = 0.0;
     for(int i =0; i<N; i++) host_sum += host_x[i];
-    double t1 = tim.lap_ms();
-
-    tim.reset();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> t1 = end - start;
 
     /*
     Reducing algorithm in GPU
@@ -49,13 +48,16 @@ int main(int argc, char const *argv[])
    for(int m = N/2; m > 0; m /= 2){
     int threads = std::min(256,m);
     int blocks = std::max(m/256,1);
+    auto start = std::chrono::high_resolution_clock::now();
 
     reduce<<<blocks, threads>>>(dev_x.data().get(), m);
+    }
     cudaDeviceSynchronize();
-    double t2 = tim.lap_ms();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> t2 = end - start;
     double gpu_sum = dev_x[0];
 
-    printf("sum of %d random numbers: host %.1f %.3f ms, GPU %.1f %.3f ms\n", N, host_sum, t1, gpu_sum, t2);
-   }
+    printf("sum of %d random numbers: host %.1f %.3d ms, GPU %.1f %.3d ms\n", N, host_sum, t1, gpu_sum, t2);
+
     return 0;
 }
