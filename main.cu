4192534a3ae7a8d0ddfc9c6ac788ab16d43878c7
claudiocamolese <claudiocamolese@gmail.com>
2025-11-17 22:32:48 +0100

    first project commit

---
+++ main.cu
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <cuda_runtime.h>
+
+/*
+main():
+    - carica il dataset
+    - dividi il dataset in train, test e val
+    - carica la rete
+    - start training con salvataggio pesi
+    - start evaluating
+    - printa risultati
+*/
+
+
+int main(){
+    printf("Initialization of the dataset");
+
+}
